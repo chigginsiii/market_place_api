75d31083b8dc2c7c7c5b428c56e15e0380678e17
chigginsiii <chiggins@chiggins.com>
2015-01-06 11:34:10 -0800

    first pass at versioning with namespacing/scoping routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json
-                              constraints: { subdomain: 'api' },
-                              path: '/'
-                            } do
-    
+  # so the application will always look under app/controller/api for the controller code
+  namespace :api,
+            # default respond_to is json...
+            defaults: { format: :json
+                        # ...when the subdomain is 'api.<app host>'...
+                        constraints: { subdomain: 'api' },
+                        # ...and serve the root path rather than defaulting to /api/
+                        path: '/'
+                      } do
+    # we're versioning the api, so this'll set resources up under /v1/ | app/controllers/api/v1/
+    scope module: :v1 do
+      
+    end
   end
 end
